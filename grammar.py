77605bf8e365e09c84b03df2316a72e8496925a6
Mantwi <michantwi28@outlook.com>
2024-05-03 12:12:22 -0400

    adding files

---
+++ grammar.py
@@ -0,0 +1,67 @@
+"""
+COMS W4705 - Natural Language Processing - Fall 2023
+Homework 2 - Parsing with Context Free Grammars 
+Daniel Bauer
+"""
+
+import sys
+from collections import defaultdict
+from math import fsum
+
+class Pcfg(object): 
+    """
+    Represent a probabilistic context free grammar. 
+    """
+
+    def __init__(self, grammar_file): 
+        self.rhs_to_rules = defaultdict(list)
+        self.lhs_to_rules = defaultdict(list)
+        self.startsymbol = None 
+        self.read_rules(grammar_file)      
+ 
+    def read_rules(self,grammar_file):
+        
+        for line in grammar_file: 
+            line = line.strip()
+            if line and not line.startswith("#"):
+                if "->" in line: 
+                    rule = self.parse_rule(line.strip())
+                    lhs, rhs, prob = rule
+                    self.rhs_to_rules[rhs].append(rule)
+                    self.lhs_to_rules[lhs].append(rule)
+                else: 
+                    startsymbol, prob = line.rsplit(";")
+                    self.startsymbol = startsymbol.strip()
+                    
+     
+    def parse_rule(self,rule_s):
+        lhs, other = rule_s.split("->")
+        lhs = lhs.strip()
+        rhs_s, prob_s = other.rsplit(";",1) 
+        prob = float(prob_s)
+        rhs = tuple(rhs_s.strip().split())
+        return (lhs, rhs, prob)
+
+    def verify_grammar(self):
+        """
+        Return True if the grammar is a valid PCFG in CNF.
+        Otherwise return False. 
+        """
+        # TODO, Part 1
+        for lh, rules_roll in self.lhs_to_rules.items():
+            if not lh.isupper():
+                return False
+            for rule in rules_list:
+                if len(rule[1]) > 2:
+                    return False
+                elif len(rule[1]) == 2 and not all(s.isupper() for s in rule[1]):
+                    return False
+            if round(fsum([rule[-1] for rule in rules_roll]), 5) != 1.0:
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1],'r') as grammar_file:
+        grammar = Pcfg(grammar_file)
+        
